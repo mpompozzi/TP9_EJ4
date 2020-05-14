bce0f77e9cb7f5b2bffc1529cebfc5e8fdfdb465
maga <maga@debian>
2020-05-13 22:39:31 -0300

    Funcion palindrome con input, no integradas las funciones

---
+++ TP4/funciones.h
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   funciones.h
+ * Author: maga
+ *
+ * Created on May 13, 2020, 9:23 PM
+ */
+
+#ifndef FUNCIONES_H
+#define FUNCIONES_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "constantes.h"
+
+char* input (void);
+int palindrome(char string[MAX_LETTERS],int cant);
+
+#endif /* FUNCIONES_H */
+

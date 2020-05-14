6d17f2881f064218dbc5ca8cb86c0c2d06dc88a9
maga <maga@debian>
2020-05-13 22:39:43 -0300

    Funcion palindrome con input, no integradas las funciones

---
+++ TP4/constantes.h
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   constantes.h
+ * Author: maga
+ *
+ * Created on May 13, 2020, 10:26 PM
+ */
+
+#ifndef CONSTANTES_H
+#define CONSTANTES_H
+
+#define OK 0
+#define FAIL 1
+#define MAX_LETTERS
+
+#endif /* CONSTANTES_H */
+
